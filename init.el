d27c563cb524f434f7cdd51f77fe5b8bef36a036
dragonly <liyilongko@163.com>
2015-08-08 04:15:54 -0700

    turn to a more elegant package discription and installatio method

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; setup el-get the super package manager
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
@@ -8,15 +9,43 @@
     (eval-print-last-sexp)))
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(require 'package) ;; You might already have this line
+
+;; add MELPA source to 'package.el' the solemn package manager
+(require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(require 'el-get-elpa)
+(unless (file-directory-p el-get-recipe-path-elpa)
+    (el-get-elpa-build-local-recipes))
 
 (desktop-save-mode 1)
+
+;; set this variable to enable auto refresh of buffers like dired-mode buffer
+(setq global-auto-revert-non-file-buffers t)
+;; setup auto refresh of files and buffers when contents of them changed
 (global-auto-revert-mode 1)
 
-(el-get-bundle elpa:better-defaults)
-(el-get-bundle ein)
-(el-get-bundle auto-complete+)
-(el-get-bundle auto-complete-emacs-lisp)
-(el-get-bundle darcula-theme)
-(require 'darcula-theme)
+;; setup el-get packages officially supported
+(setq el-get-packages
+      '(
+        el-get
+        ein
+        auto-complete+
+        auto-complete-emacs-lisp))
+
+;; setup packages out of official el-get repoOC`                                    
+(setq el-get-sources
+      '(
+        (:name firecode-theme
+               :type elpa)
+        (:name better-defaults
+               :type elpa)))
+
+;; merge all packages from different sources
+(setq el-get-packages
+      (append el-get-packages
+;              (mapcar 'el-get-as-symbol ;this seems unimportant?
+                      (mapcar 'el-get-source-name el-get-sources)))
+(el-get 'sync el-get-packages)
+
+(load-theme 'firecode t)
